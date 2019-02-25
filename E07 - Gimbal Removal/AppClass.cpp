64be8ae0d0b64203ac1edfe936a275225956c6fa
ekt6170 <ekt6170@g.rit.edu>
2019-02-25 17:41:25 -0500

    E07 Deliverable

--- E07 - Gimbal Removal/AppClass.cpp
+++ E07 - Gimbal Removal/AppClass.cpp
@@ -25,14 +25,15 @@ void Application::Display(void)
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 
-	m_m4Model = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
-	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
+	//declaring quaternion objects for the x, y, and z axisesseses
+	glm::quat q1 = glm::angleAxis(glm::radians(m_v3Rotation.x), AXIS_X);
+	glm::quat q2 = glm::angleAxis(glm::radians(m_v3Rotation.y), AXIS_Y);
+	glm::quat q3 = glm::angleAxis(glm::radians(m_v3Rotation.z), AXIS_Z);
 
-	//m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), vector3(1.0f));
-	//m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
+	m_qOrientation = m_qOrientation * (q1*q2*q3);
+	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
 	
+	m_v3Rotation = vector3(0, 0, 0);
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
 	
